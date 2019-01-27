05f57943a83367c5a7f2282da2cb8a1eb57c7c19
Oko Dev <oko.webdev@gmail.com>
2019-01-27 16:01:34 +0300

    corecache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use goodmade\App;
+use goodmade\Cache;
 
 class MainController extends AppController {
 
@@ -13,6 +14,10 @@ class MainController extends AppController {
         $name = 'TEST';
         $age = 34;
         $names = ['Gandalf', 'Qui-Gon Jinn',];
+        $cache = Cache::instance();
+//        $cache->set('test',$names);
+        $data =$cache->get('test');
+        debug($data);
         $this->set(compact('name', 'age', 'names', 'posts'));    }
 
 
